b652e385aa997f6f5fc5d7839828dcf582ced4ec
Yousef M <yousefmalsaadan@gmail.com>
2023-05-04 12:50:23 +0300

    lap function soltion

---
+++ main.py
@@ -0,0 +1,14 @@
+def series_number(num: int):
+    ''' this function for print a serises of number in inverted triangle shape. '''
+    for i in range(0, num + 1):
+        for j in range(num - i, 0, -1):
+            print(j, end = " ")
+        print("\n")
+
+while True:
+    user_input = input("Enter a number: ")
+    if user_input.isdigit():
+        series_number(int(user_input))
+        break
+    else:
+        print("type a integer number pleas!!")
