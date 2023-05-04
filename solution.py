1f6086df6e075b7ccd563a0d1558fc354a1f498b
1947860@uj.edu.sa <1947860@uj.edu.sa>
2023-05-04 12:23:00 +0300

    solution

--- solution.py
+++ solution.py
@@ -1,1 +1,20 @@
-https://github.com/faisal-alhussain/LAB_FUNCTIONS_1.git
+def rows(Number:int):
+    '''
+creating pettren of 5 rows as showen 
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+    '''
+    for i in range(Number,0,-1):
+        for j in range(i,0,-1):
+            if j<= Number:
+                print(j,end=" ")
+            else:
+                Number=-1
+        print(" ")
+
+
+rows(5)
+print(rows.__doc__)
