f9251623cab274793471bbc7b04f3c312bb050dd
Rayan Hantoul <ryan-055@outlook.com>
2023-05-04 13:03:52 +0300

    update solved

--- Lap_functions.py
+++ Lap_functions.py
@@ -1,4 +1,5 @@
 def p_number(numbers:int):
+    '''Document the newly created function. describe what it does, then print the documentation'''
     c_numbers = range(numbers,0,-1)
     for n in c_numbers :
         for a in range(numbers,0,-1):
