2334db9cff298aec7a7565b23fa3dbda038f4eb6
hess-01 <hessah-14@hotmail.com>
2023-05-07 09:44:49 +0300

    fuction2

---
+++ fuction 1 solved.py
@@ -0,0 +1,8 @@
+#Create a function that takes 1 parameter of type int , then it prints out the result 
+#formatted like the following patter (if we give it 5 for example):
+
+def print_pattern(num):
+    for i in range(1, num+1):
+        print ("1 ")
+        for I in range(num +1):
+            print 
