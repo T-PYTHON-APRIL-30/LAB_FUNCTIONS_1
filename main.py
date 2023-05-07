c839d9b1c351ea89b10c0bac8821a269be4e81a8
SulimanBinM <sbinmohammed_1@icloud.com>
2023-05-07 11:50:49 +0300

    Print the doc.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def countDown(x) -> int :
-
+    '''This function it will prints out the result formatted ! '''
     for i in range(x,0,-1):
         for j in range(i,0,-1) :
             print(j,end=" ")
         print()
+    
+print(countDown.__doc__)
 
 x = int(input("Enter a number: "))
+
 countDown(x)
