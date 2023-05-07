2b2a333dd420d6a6cde1e48b5397b0cde21cd69f
Abdulaziz <Abdulaziz-A1997@hotmail.com>
2023-05-07 17:13:27 +0300

    Documenting has been added

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 def one_parameter (numb:int)->int:
+    '''printing the numbers from 5 to 1, and removing a number each line'''
     for numb in range (numb,0,-1):
         for i in range (numb,0,-1):
             print(i,end ="")
         print()    
-one_parameter(5)            
+one_parameter(5)
